3d00ff6ba4a2438dc498cfc2e5e8d5a599e3f865
Seth <18116602+sethkor@users.noreply.github.com>
2019-04-25 17:47:26 +1000

    Test Linux Brew

--- Formula/lexbelt.rb
+++ Formula/lexbelt.rb
@@ -8,12 +8,16 @@ class Lexbelt < Formula
     url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Darwin_amd64.tar.gz"
     sha256 "b99ffed098ebb1411f04abbea5da46274e2bcf5f821b3670240e30ecc89e5ffa"
   elsif OS.linux?
-#  
-
-     if OS.bits() == 64
+  
+    case RbConfig::CONFIG["host_cpu"]
+    when "aarch64"
+      url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Linux_arm64.tar.gz"
+      sha256 "1c4fb160b741ebefbf35fedff8f569c494ddfdae52e0f9f454618891f7ed7183"   
+    when "x86_64" 
       url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Linux_amd64.tar.gz"
       sha256 "0910b4419f9739457552bb567774bcf3c89c5c9c5060ceec74d28af55a54092d"
-   end
+    else
+      ohdie "Architecture not supported by this forumla"
   end
 
   def install
