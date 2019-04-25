e57bcc13506f7d7933cde6fc0959e32000ac808d
Seth <18116602+sethkor@users.noreply.github.com>
2019-04-25 15:09:08 +1000

    Test Linux Brew

--- Formula/lexbelt.rb
+++ Formula/lexbelt.rb
@@ -9,11 +9,13 @@ class Lexbelt < Formula
     sha256 "b99ffed098ebb1411f04abbea5da46274e2bcf5f821b3670240e30ecc89e5ffa"
   elsif OS.linux?
   
-    puts OS.bits?
+    puts OS.linux
+    if OS.bits == 64 then
+
 #     if OS.bits == 64
-#       url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Linux_amd64.tar.gz"
-#       sha256 "0910b4419f9739457552bb567774bcf3c89c5c9c5060ceec74d28af55a54092d"
-#    end
+      url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Linux_amd64.tar.gz"
+      sha256 "0910b4419f9739457552bb567774bcf3c89c5c9c5060ceec74d28af55a54092d"
+   end
   end
 
   def install
