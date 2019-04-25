cb3abd4b22737e77e6b4ebfafeb545417e9f7720
Seth <18116602+sethkor@users.noreply.github.com>
2019-04-25 14:08:22 +1000

    Test Linux Brew

--- Formula/lexbelt.rb
+++ Formula/lexbelt.rb
@@ -2,9 +2,15 @@
 class Lexbelt < Formula
   desc "cli tools for AWS Lex"
   homepage "https://github.com/sethkor/lexbelt/"
-  url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Darwin_amd64.tar.gz"
   version "0.0.2"
-  sha256 "b99ffed098ebb1411f04abbea5da46274e2bcf5f821b3670240e30ecc89e5ffa"
+
+  if OS.mac?
+    url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Darwin_amd64.tar.gz"
+    sha256 "b99ffed098ebb1411f04abbea5da46274e2bcf5f821b3670240e30ecc89e5ffa"
+  elsif OS.linux?
+    url "https://github.com/sethkor/lexbelt/releases/download/0.0.2/lexbelt_0.0.2_Linux_amd64.tar.gz"
+    sha256 "0910b4419f9739457552bb567774bcf3c89c5c9c5060ceec74d28af55a54092d"
+  end
 
   def install
     bin.install "lexbelt"
