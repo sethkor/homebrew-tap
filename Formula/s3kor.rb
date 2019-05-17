06a6e175c8b33494589fff0b8a85a26e225b634d
Seth Van Buren <sethvanburen@Versents-MacBook-Pro.local>
2019-05-17 16:22:11 +1000

    Add version 3 checksums

--- Formula/s3kor.rb
+++ Formula/s3kor.rb
@@ -4,17 +4,17 @@ class S3kor < Formula
   version "0.0.2"
 
   if OS.mac?
-    url "https://github.com/sethkor/s3kor/releases/download/0.0.2/s3kor_0.0.2_Darwin_amd64.tar.gz"
-    sha256 "05a77453ed85f79b3e82d4c8a5a39d4d2d84e3fbf08efb9947dee6b5e19d948f"
+    url "https://github.com/sethkor/s3kor/releases/download/0.0.3/s3kor_0.0.3_Darwin_amd64.tar.gz"
+    sha256 "5a128b7d1c15e268369f2863da07f1a1a9272cf6a9faba611613909d6a8f70b3"
   elsif OS.linux?
   
     case RbConfig::CONFIG["host_cpu"]
     when "aarch64"
-      url "https://github.com/sethkor/s3kor/releases/download/0.0.2/s3kor_0.0.2_Linux_arm64.tar.gz"
+      url "https://github.com/sethkor/s3kor/releases/download/0.0.3/s3kor_0.0.3_Linux_arm64.tar.gz"
       sha256 "5ed8469adf00735012f17eae70db49dae6adb9334a701707d7859ba69a8aa6de"   
     when "x86_64" 
-      url "https://github.com/sethkor/s3kor/releases/download/0.0.2/s3kor_0.0.2_Linux_amd64.tar.gz"
-      sha256 "f9107a6007ff0f2502281e7d4cb84257f356e20c17f032b7419843caaaaafb65"
+      url "https://github.com/sethkor/s3kor/releases/download/0.0.3/s3kor_0.0.3_Linux_amd64.tar.gz"
+      sha256 "0a6826c1e39fd5f5d5eaeff8837ea2fb464500a00ce6ffae2b17172135a9ece9"
     else
       ohdie "Architecture not supported by this forumla"
     end
