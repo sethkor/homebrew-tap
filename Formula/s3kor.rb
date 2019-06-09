e54f4c5a789d4e8d5e3a3a786b9b25de25bff04c
Seth <18116602+sethkor@users.noreply.github.com>
2019-06-10 00:10:48 +1000

    Fix typo

--- Formula/s3kor.rb
+++ Formula/s3kor.rb
@@ -5,16 +5,16 @@ class S3kor < Formula
 
   if OS.mac?
     url "https://github.com/sethkor/s3kor/releases/download/0.0.15/s3kor_0.0.15_Darwin_amd64.tar.gz"
-    sha256 "bc54417a14dc9538bef0e1f7ab269477503221c7b7c8ac4116f55c179c082397"
+    sha256 "259d477329ea59a6333a909eeab5725c189d6b72fec9055ec5f55c13cd373da5"
   elsif OS.linux?
   
     case RbConfig::CONFIG["host_cpu"]
     when "aarch64"
       url "https://github.com/sethkor/s3kor/releases/download/0.0.15/s3kor_0.0.15_Linux_arm64.tar.gz"
-      sha256 "a92a2c7e896d3dea35ff127c531175bfbb78a1a05eaccaa7366c188c9a4396c5"   
+      sha256 "d0ddf79778b8dc46bfa302a6e8bcc25231d8ee935c97ecf071efefc65b7f9abd"   
     when "x86_64" 
       url "https://github.com/sethkor/s3kor/releases/download/0.0.15/s3kor_0.0.15_Linux_amd64.tar.gz"
-      sha256 "e15445b8bd8ef891d491d95844c276e4ec5296e7c775ef8a692b7f8dac7f25e1"
+      sha256 "d1d25f6ac9af7c56188375b718ad15e814daebcf57cdccd139117d7d514e22a6"
     else
       ohdie "Architecture not supported by this forumla"
     end
