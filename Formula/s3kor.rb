7dd71a4a09606913d13c5fa5dd61ffb90f9a4e2b
Seth <18116602+sethkor@users.noreply.github.com>
2019-06-09 17:31:11 +1000

    Fix typo in formula

--- Formula/s3kor.rb
+++ Formula/s3kor.rb
@@ -11,9 +11,9 @@ class S3kor < Formula
     case RbConfig::CONFIG["host_cpu"]
     when "aarch64"
       url "https://github.com/sethkor/s3kor/releases/download/0.0.13/s3kor_0.0.13_Linux_arm64.tar.gz"
-      sha256 af310595a4ab99e28b25a57d031922df5e0fb518dbcd71a26dd2d97123ee405a"   
+      sha256 "af310595a4ab99e28b25a57d031922df5e0fb518dbcd71a26dd2d97123ee405a"   
     when "x86_64" 
-      url "https://github.com/sethkor/s3kor/releases/download/0.0.13/s3kor_0.0.0.0.13_Linux_amd64.tar.gz"
+      url "https://github.com/sethkor/s3kor/releases/download/0.0.13/s3kor_0.0.13_Linux_amd64.tar.gz"
       sha256 "e576f5bb769921e8f5e8442b26298c4df5e6bde9dcab6ecf706d539f8a57e18a"
     else
       ohdie "Architecture not supported by this forumla"
