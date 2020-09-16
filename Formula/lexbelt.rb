cbeee00e4a20df444ff824e96f5c3972eaad3e6b
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-16 21:12:43 +1000

    Brew formula update for lexbelt version v0.0.5

--- Formula/lexbelt.rb
+++ Formula/lexbelt.rb
@@ -2,24 +2,24 @@
 class Lexbelt < Formula
   desc "Fast S3 Tools built in GoLang Using Multiparts and Concurrency"
   homepage ""
-  version "0.0.4"
+  version "0.0.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sethkor/lexbelt/releases/download/v0.0.4/lexbelt_0.0.4_Darwin_amd64.tar.gz"
-    sha256 "afbd89395622387b34c1d558c79e781b37909c5a111c9f67f27b2e70f7c55263"
+    url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Darwin_amd64.tar.gz"
+    sha256 "a1a296a9b0a31853e4ad952a7a03dc770d7f1a61b0911fc99eb6d13ff2f403e6"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sethkor/lexbelt/releases/download/v0.0.4/lexbelt_0.0.4_Linux_amd64.tar.gz"
-      sha256 "75b6ebea19c2c533cba1402e7471319dc5485bcbdd0437a247dd3fb011f3f593"
+      url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Linux_amd64.tar.gz"
+      sha256 "6331bdfd12e256bdd4fdcfb7e3b9806700dd5d6b418e451c987bff08b677bf0b"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.4/lexbelt_0.0.4_Linux_arm64.tar.gz"
-        sha256 "b92665f5befb06cc5431cb5f1413ce027c45fe0746faf6df9bcc53a9a6e75c94"
+        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Linux_arm64.tar.gz"
+        sha256 "c0d4bf6c59efbc05c227c8c8e4cb2d248dd2374e1a81ba4af639c7e6bcc9f55d"
       else
-        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.4/lexbelt_0.0.4_Linux_armv6.tar.gz"
-        sha256 "6052f57450584a437f65c28f0a48304ec23b8a74ee73ffcd54089712ca8e670e"
+        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Linux_armv6.tar.gz"
+        sha256 "9cb3cf6a841abfa934bdcd7dca5ecfbb0bedc65cba921ae0070401e304fffe2f"
       end
     end
   end
