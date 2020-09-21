a14a95119de754fe95d562cce600489069f8cee7
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-21 16:57:09 +1000

    Brew formula update for lexbelt version v0.0.6

--- Formula/lexbelt.rb
+++ Formula/lexbelt.rb
@@ -2,24 +2,24 @@
 class Lexbelt < Formula
   desc "Fast S3 Tools built in GoLang Using Multiparts and Concurrency"
   homepage ""
-  version "0.0.5"
+  version "0.0.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Darwin_amd64.tar.gz"
-    sha256 "a1a296a9b0a31853e4ad952a7a03dc770d7f1a61b0911fc99eb6d13ff2f403e6"
+    url "https://github.com/sethkor/lexbelt/releases/download/v0.0.6/lexbelt_0.0.6_Darwin_amd64.tar.gz"
+    sha256 "1642a7b5bee8b6c0e2b1111bc0af1219570d41750130c39e14cfa85cd0219e65"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Linux_amd64.tar.gz"
-      sha256 "6331bdfd12e256bdd4fdcfb7e3b9806700dd5d6b418e451c987bff08b677bf0b"
+      url "https://github.com/sethkor/lexbelt/releases/download/v0.0.6/lexbelt_0.0.6_Linux_amd64.tar.gz"
+      sha256 "8ad8b6128f16c64f869aa23386ad3df9c128afff392e69cc31eb7513b29e7f1e"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Linux_arm64.tar.gz"
-        sha256 "c0d4bf6c59efbc05c227c8c8e4cb2d248dd2374e1a81ba4af639c7e6bcc9f55d"
+        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.6/lexbelt_0.0.6_Linux_arm64.tar.gz"
+        sha256 "002420c4f6442e2c32de0cd0e0465f6e60b11e2e7a752ca6dad3d5ee4f213981"
       else
-        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.5/lexbelt_0.0.5_Linux_armv6.tar.gz"
-        sha256 "9cb3cf6a841abfa934bdcd7dca5ecfbb0bedc65cba921ae0070401e304fffe2f"
+        url "https://github.com/sethkor/lexbelt/releases/download/v0.0.6/lexbelt_0.0.6_Linux_armv6.tar.gz"
+        sha256 "e270697d5155c96ba1c3d989dc11252e4ad0a17045f5bb3325ba5d826ce85b03"
       end
     end
   end
