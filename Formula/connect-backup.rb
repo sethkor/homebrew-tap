b6978e3e14393b779dd3e183ad9e9a936efb921c
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-17 23:51:06 +1100

    Brew formula update for connect-backup version v0.0.16

--- Formula/connect-backup.rb
+++ Formula/connect-backup.rb
@@ -2,24 +2,24 @@
 class ConnectBackup < Formula
   desc "Fast S3 Tools built in GoLang Using Multiparts and Concurrency"
   homepage ""
-  version "0.0.15"
+  version "0.0.16"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sethkor/connect-backup/releases/download/v0.0.15/connect-backup_0.0.15_Darwin_amd64.tar.gz"
-    sha256 "1e00f7fdf4e8d506983bd091e9834ea559775524e16b7254db2c8bc81de0356f"
+    url "https://github.com/sethkor/connect-backup/releases/download/v0.0.16/connect-backup_0.0.16_Darwin_amd64.tar.gz"
+    sha256 "e68358c20c0e50cd6a1fef278be70d1763d6021c134b075040f90812011b0e84"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sethkor/connect-backup/releases/download/v0.0.15/connect-backup_0.0.15_Linux_amd64.tar.gz"
-      sha256 "9611f8f2dcf5895c71923abc8b8c0699a9ad96c1c59c3c9e679667b38807d935"
+      url "https://github.com/sethkor/connect-backup/releases/download/v0.0.16/connect-backup_0.0.16_Linux_amd64.tar.gz"
+      sha256 "1372cb5241184f213ce1a3eec475d7a7aff794942fd192c826c390aa4c12be26"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/sethkor/connect-backup/releases/download/v0.0.15/connect-backup_0.0.15_Linux_arm64.tar.gz"
-        sha256 "6043b620a19d564a6b412cdedd0600afb2b9b509f038b6441183588e9a3de9df"
+        url "https://github.com/sethkor/connect-backup/releases/download/v0.0.16/connect-backup_0.0.16_Linux_arm64.tar.gz"
+        sha256 "63e2e16ee47a20973d5274d78baa0dfb906041add7e01c0cdd640185bf5b2475"
       else
-        url "https://github.com/sethkor/connect-backup/releases/download/v0.0.15/connect-backup_0.0.15_Linux_armv6.tar.gz"
-        sha256 "a9dbb83b4c99596085bff2c46e4ae8c1db4d06eacfd58644b8c04c4f3f7625be"
+        url "https://github.com/sethkor/connect-backup/releases/download/v0.0.16/connect-backup_0.0.16_Linux_armv6.tar.gz"
+        sha256 "4a3b406247f73d9f6d25b88288c5d9a086ea12adb3e572ce152f384b3392f54d"
       end
     end
   end
